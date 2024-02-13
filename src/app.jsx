96c9588782bb43063e1e76e1d263e968c293249b
marciobadias <marcio.b.a.dias@gmail.com>
2024-02-13 12:32:38 -0300

    ✅ Criando hooks personalizados

--- src/app.jsx
+++ src/app.jsx
@@ -1,55 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useMovies } from './hooks'
 import { SearchMenu } from './components/SearchMenu'
 import { Main } from './components/Main'
 
-const apiKey = import.meta.env.VITE_API_KEY
-
 const App = () => {
-  const [movies, setMovies] = useState([])
-
-  useEffect(() => {
-    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=Matrix`)
-      .then((r) => r.json())
-      .then((data) =>
-        setMovies(
-          data.Search.map((movie) => ({
-            id: movie.imdbID,
-            title: movie.Title,
-            year: movie.Year,
-            poster: movie.Poster,
-          })),
-        ),
-      )
-      .catch(console.log)
-  }, [])
-
-  const handleSearchMovie = (e) => {
-    e.preventDefault()
-    const { searchMovie } = e.target.elements
-
-    if (searchMovie.value.length < 2) {
-      return
-    }
-
-    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchMovie.value}`)
-      .then((r) => r.json())
-      .then((data) =>
-        setMovies(
-          data.Search.map((movie) => ({
-            id: movie.imdbID,
-            title: movie.Title,
-            year: movie.Year,
-            poster: movie.Poster,
-          })),
-        ),
-      )
-      .catch(console.log)
-  }
+  const { movies, handleSearchMovie } = useMovies()
 
   return (
     <>
       <SearchMenu movies={movies} onSearchMovie={handleSearchMovie} />
-      <Main movie={movies} apiKey={apiKey} />
+      <Main movie={movies} />
     </>
   )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,53 +1,20 @@
-import { useState } from 'react'
 import { HistoricMoviesData } from '../HistoricMoviesData'
 import { HistoricMoviesList } from '../HistoricMoviesList'
 import { ListBox } from '../ListBox'
 import { MovieDetails } from '../MovieDetails'
 import { MoviesList } from '../MoviesList'
+import { useWatchedMovies, useClickedMovie } from '../../hooks'
 
-const Main = ({ movie, apiKey }) => {
-  const [clickedMovie, setClickedMovie] = useState(null)
-  const [wacthedMovies, setWacthedMovies] = useState([])
+const Main = ({ movie }) => {
+  const { wacthedMovies, setWacthedMovies, handleClickBtnDelete } =
+    useWatchedMovies()
 
-  const handleClickBtnDelete = (id) =>
-    setWacthedMovies((prev) => prev.filter((p) => p.id !== id))
-  const handleClickBtnBack = () => setClickedMovie(null)
-  const handleClickedMovie = (currentClickedMovie) => {
-    const prevCLickedMove = clickedMovie
-    if (prevCLickedMove?.id === currentClickedMovie.id) {
-      setClickedMovie(null)
-    }
-
-    fetch(
-      `https://www.omdbapi.com/?apikey=${apiKey}&i=${currentClickedMovie.id}`,
-    )
-      .then((r) => r.json())
-      .then((movie) =>
-        setClickedMovie({
-          id: movie.imdbID,
-          title: movie.Title,
-          year: movie.Year,
-          imdbRate: movie.imdbRating,
-          runtime: movie.Runtime,
-          poster: movie.Poster,
-          plot: movie.Plot,
-          actors: movie.Actors,
-          director: movie.Director,
-          released: movie.Released,
-          genre: movie.Genre,
-        }),
-      )
-      .catch(console.log)
-  }
-  const handleClickSubmitRating = (e) => {
-    e.preventDefault()
-    const { rating } = e.target.elements
-    setWacthedMovies((prev) => [
-      ...prev,
-      { ...clickedMovie, userRating: rating.value },
-    ])
-    setClickedMovie(null)
-  }
+  const {
+    clickedMovie,
+    handleClickBtnBack,
+    handleClickedMovie,
+    handleClickSubmitRating,
+  } = useClickedMovie(setWacthedMovies)
 
   return (
     <main className="main">
